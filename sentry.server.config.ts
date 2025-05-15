13ecca497c9cd4b65d29df24e222ec899fa86a46
betegon <miguelbetegongarcia@gmail.com>
2025-05-15 17:29:46 +0200

    test ai sdk tracing

---
+++ sentry.server.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://6b73db991163129e1fd5a8f85ec67c33@o1.ingest.us.sentry.io/4509326969995264",
+  spotlight: process.env.NODE_ENV === 'development',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
